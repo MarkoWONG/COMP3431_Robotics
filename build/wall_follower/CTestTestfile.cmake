2837ad410e036616c10ad2e4cbf516cf8edffec6
Marko Wong <Nfslolmiku@gmail.com>
2023-09-19 23:58:09 +1000

    added starter code

---
+++ build/wall_follower/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marko/comp3431_workspace/src/wall_follower/wall_follower
+# Build directory: /home/marko/comp3431_workspace/build/wall_follower
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
