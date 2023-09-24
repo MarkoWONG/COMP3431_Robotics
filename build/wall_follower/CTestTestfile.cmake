76bcde1fd27bf0ee61fef03056c559b06a3070ba
Marko Wong <Nfslolmiku@gmail.com>
2023-09-24 20:57:44 +1000

    Delete build directory

--- build/wall_follower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marko/comp3431_workspace/src/wall_follower/wall_follower
-# Build directory: /home/marko/comp3431_workspace/build/wall_follower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
