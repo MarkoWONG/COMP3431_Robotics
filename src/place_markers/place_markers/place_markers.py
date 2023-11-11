657c76522876ef959cdffa51c9cd550fe64450f3
Raymond Pham <z5310070@ad.unsw.edu.au>
2023-11-11 10:31:59 +0000

    making changes

--- src/place_markers/place_markers/place_markers.py
+++ src/place_markers/place_markers/place_markers.py
@@ -14,6 +14,7 @@ import rclpy.qos
 from pyquaternion import Quaternion
 from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
+from nav_msgs.msg import Odometry
 
 import cv2 # OpenCV library
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
@@ -31,6 +32,11 @@ class ImageSubscriber(Node):
   YELLOW = 3
   MAX_AREA_DETECTION_THRESHOLD = 1100
   MIN_AREA_DETECTION_THRESHOLD = 200
+
+  BLUE_PINK = False
+  PINK_BLUE = False
+  GREEN_PINK = False
+  YELLOW_PINK = False
   """
   Create an ImageSubscriber class, which is a subclass of the Node class.
   """
@@ -41,8 +47,8 @@ class ImageSubscriber(Node):
     # Initiate the Node class's constructor and give it a name
     super().__init__('image_subscriber')
 
-    self.marker_list = MarkerArray();
-    self.marker_list.markers = [];
+    self.marker_list = MarkerArray()
+    self.marker_list.markers = []
       
     # Create the subscriber. This subscriber will receive an Image
     # from the video_frames topic. The queue size is 10 messages.
@@ -53,26 +59,32 @@ class ImageSubscriber(Node):
       self.listener_callback, 
       10)
     self.subscription # prevent unused variable warning
-      
+
+    # Subscribe to odometry
+    self.subscription = self.create_subscription(
+      Odometry,
+      '/odom',
+      self.listener_callback,
+      10)
+    
+    self.position = None
+
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
 
     #For storing objects detected by the robot's camera
-    self.detected_objects = [];
-    self.image_size = None;
-
-    # position listener
-    self.qos = rclpy.qos.QoSProfile(
-        reliability=rclpy.qos.QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
-        history=rclpy.qos.QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
-        depth=1
-    )
+    self.detected_objects = []
 
     # transform lisnter
     self.tf_buffer = Buffer()
     self.tf_listener = TransformListener(self.tf_buffer, self)
+
     self.plot_publisher = self.create_publisher(MarkerArray, "visualization_marker_array", 10)
 
+  def odom_callback(self, data):
+    pos = data.pose.pose.position
+    self.position = pos
+    (posx, posy, posz) = (pos.x, pos.y, pos.z)
 
   def listener_callback(self, data):
     """
@@ -91,15 +103,13 @@ class ImageSubscriber(Node):
     hsv_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
     self.image_size = hsv_frame.shape
     
-    #We only need to worry about blue, green and yellow because all the markers are half pink
+    #Detect objects runs a connected component analysis and adds the blobs to a list "detected_objects"
     self.detect_objects(hsv_frame, current_frame)
     self.calcDistanceAndPublish()
 
     # Display camera image
     cv2.namedWindow("camera")
     cv2.imshow("camera", current_frame)
-    # Display masked image
-    # cv2.imshow("mask", mask)
     
     cv2.waitKey(1)
 
@@ -139,9 +149,6 @@ class ImageSubscriber(Node):
     green_objects = cv2.connectedComponentsWithStats(green_mask, 4, cv2.CV_32S)
     (numLabelsG, labelsG, statsG, centroidsG) = green_objects
 
-    # Print statistics for each blob (connected component)
-    # use these statistics to find the bounding box of each blob
-    # and to line up laser scan with centroid of the blob
     for i in range(1, numLabels):
       x = stats[i, cv2.CC_STAT_LEFT]
       y = stats[i, cv2.CC_STAT_TOP]
@@ -149,7 +156,6 @@ class ImageSubscriber(Node):
       h = stats[i, cv2.CC_STAT_HEIGHT]
       area = stats[i, cv2.CC_STAT_AREA]
         
-      #If the area of the blob is more than 250 pixels:
       if area >= self.MIN_AREA_DETECTION_THRESHOLD and area <= self.MAX_AREA_DETECTION_THRESHOLD:
         (centroid_x1, centroid_y1) = centroids[i]
         pink_on_top = True
@@ -220,85 +226,61 @@ class ImageSubscriber(Node):
         self.detected_objects.append({"color": color, "pink_on_top": pink_on_top, "x": x, "y": y, "w": w, "h": h, "area": area, "centroid": centroids[i]})
         print(f"color: {color}, pink on top: {pink_on_top}, width: {w}, height: {h}, area: {area}")
   
-  def check_existing_markers(self, point, new_color):
-    for marker in self.marker_list.markers:
-      if math.sqrt((point[0] - marker.pose.position.x)**2 + \
-        (point[1] - marker.pose.position.y)**2) <= 0.5:
-        r = marker.color.r * 255
-        g = marker.color.g * 255
-        b = marker.color.b * 255
-        
-        if new_color == self.BLUE and abs(r) <= 0.1 and abs(g - 191) <= 0.1 and abs(b - 255) < 0.1:
-          return True
-        elif new_color == self.YELLOW and abs(r - 255) <= 0.1 and abs(g - 234) <= 0.1 and abs(b) < 0.1:
-          return True
-        elif new_color == self.GREEN and abs(r) <= 0.1 and abs(g - 100) <= 0.1 and abs(b) < 0.1:
-          return True
-    return False
-
-  def transform_frame(self, target, source , translation, quaternion):
-      try:
-        transform = self.tf_buffer.lookup_transform(target_frame=target, source_frame=source, time=rclpy.time.Time()).transform
-        translation[0] = translation[0] + transform.translation.x
-        translation[1] = translation[1] + transform.translation.y
-        translation[2] = translation[2] + transform.translation.z
-        quaternion[0] = transform.rotation.w
-        quaternion[1] = quaternion[1] + transform.rotation.x
-        quaternion[2] = quaternion[2] + transform.rotation.y
-        quaternion[3] = quaternion[3] + transform.rotation.z
-        return (translation, quaternion)
-      except Exception:
-        return ([0],[0])
-
   def calcDistanceAndPublish(self):
     if len(self.detected_objects) == 0: 
       return 
-
-    robot_currX = 0;
-    robot_currY = 0;
-    robot_currZ = 0;
     
-    object = self.detected_objects[0]
-    object_height = object["h"]
-    object_width = object["w"]
-    object_fromLeft = object["x"]
-    object_fromTop = object["y"]
-    object_area = object["area"]
-    object_centroid = object["centroid"]
-    object_color = object["color"]
-    pink_on_top = object["pink_on_top"]
+    # Odometry Position
+    odom_pos = self.position
+    (posx, posy, posz) = (odom_pos.x, odom_pos.y, odom_pos.z)
 
-    dist_objectToMarker = self.distMarkerToCamera(object_height)
-    object_realHeight = 200 # change to 0.2 if needed
-    # object_pixelHeight = object_height * 0.2646
-    similarTriangleRatio = object_realHeight / object_height
-
-    # real_distance = object_realHeight / object_pixelHeight * dist_objectToMarker
-    real_distance = dist_objectToMarker # * similarTriangleRatio
-    rel_xToCenter = object_fromLeft - self.image_size[0] / 2.0
-    # real_xToCenter = object_realHeight / object_pixelHeight * rel_xToCenter
+    # Transform odometry position to map
+    (translation, rotation) = self.tf_listener('/odom', '/map', rclpy.time.Time())
     
-    # cylinder_absX = robot_currX + real_xToCenter
-    cylinder_absX = rel_xToCenter * similarTriangleRatio
-    cylinder_absY = math.sqrt(math.pow(real_distance, 2) - math.pow(cylinder_absX, 2))      
-    cylinder_absZ = 0
+    for object in self.detected_objects:
+      color = object["color"]
+      pink_on_top = object["pink_on_top"]
+
+      if (color == self.BLUE and pink_on_top == False and self.BLUE_PINK == False):
+        self.BLUE_PINK = True
+        self.add_new_point()
+      elif (color == self.BLUE and pink_on_top == True and self.PINK_BLUE == False):
+        self.PINK_BLUE = True
+        self.add_new_point()
+      elif (color == self.GREEN and self.GREEN_PINK == False):
+        self.GREEN_PINK = True
+        self.add_new_point()
+      if (color == self.BLUE and pink_on_top == False and self.BLUE_PINK == False):
+        self.BLUE_PINK = True
+        self.add_new_point()
+
+
+    # # real_distance = object_realHeight / object_pixelHeight * dist_objectToMarker
+    # real_distance = dist_objectToMarker # * similarTriangleRatio
+    # rel_xToCenter = object_fromLeft - self.image_size[0] / 2.0
+    # # real_xToCenter = object_realHeight / object_pixelHeight * rel_xToCenter
     
-    obj_in_cam = [cylinder_absY, cylinder_absX, 0]
-    # obj_in_cam = [0, 0, 0]
-    translation = [0,0,0]
-    quaternion = [1,0,0,0]
-    translation, quaternion = self.transform_frame("map", "camera_link", translation, quaternion)
-    if (len(translation) != 3):
-      return
+    # # cylinder_absX = robot_currX + real_xToCenter
+    # cylinder_absX = rel_xToCenter * similarTriangleRatio
+    # cylinder_absY = math.sqrt(math.pow(real_distance, 2) - math.pow(cylinder_absX, 2))      
+    # cylinder_absZ = 0
+    
+    # obj_in_cam = [cylinder_absY, cylinder_absX, 0]
+    # # obj_in_cam = [0, 0, 0]
+    # translation = [0,0,0]
+    # quaternion = [1,0,0,0]
+    # translation, quaternion = self.transform_frame("map", "camera_link", translation, quaternion)
+    # if (len(translation) != 3):
+    #   return
 
-    my_quater = Quaternion(quaternion[0], quaternion[1], quaternion[2],quaternion[3])
-    rotation = my_quater.rotate(obj_in_cam)
-    final_coordinate = [0,0,0]
-    final_coordinate[0] = rotation[0] + translation[0]
-    final_coordinate[1] = rotation[1] + translation[1]
-    final_coordinate[2] = rotation[2] + translation[2]
+    # my_quater = Quaternion(quaternion[0], quaternion[1], quaternion[2],quaternion[3])
+    # rotation = my_quater.rotate(obj_in_cam)
+    # final_coordinate = [0,0,0]
+    # final_coordinate[0] = rotation[0] + translation[0]
+    # final_coordinate[1] = rotation[1] + translation[1]
+    # final_coordinate[2] = rotation[2] + translation[2]
 
-    self.add_new_point(final_coordinate, object_color, pink_on_top)
+    # self.add_new_point(final_coordinate, object_color, pink_on_top)
   
   def generate_marker(self, coordinate, color, pink_on_top):
     # down cylinder
@@ -372,23 +354,7 @@ class ImageSubscriber(Node):
     self.generate_marker(coordinate, color, pink_on_top)
     print(f"-----------Current length{len(self.marker_list.markers)}----------")
     self.plot_publisher.publish(self.marker_list)
-
-  def showDistance(self, marker_height):
-    if (marker_height >= 15 and marker_height <= 65):
-      print(f'Distance to marker is {self.distMarkerToCamera(marker_height)}')
-    else:
-      print(f"Too close or far to marker, Distance won't be accurate. Distance is less than 200mm or greater than 500mm")
       
-  def distMarkerToCamera(self, marker_height):
-    # Quadratic regression equation distance = a*x^2 + b*x + c
-    a = 0.000561
-    b = -0.0622
-    c = 2.22
-    distance = (a*(marker_height**2)) - (b*marker_height) + c 
-    # self.showDistance(marker_height)
-    return distance # return 1 for debugging
-
-
 def main(args=None):
 
   # Initialize the rclpy library
